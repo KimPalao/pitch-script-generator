7e7b138064987f723a7c7aec28c50abe5b452689
Kim Palao <kimfarhantpalao@gmail.com>
2024-07-14 18:13:27 +0800

    Add buttons to nav

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -3,9 +3,15 @@ import styles from '@/app/ui/nav.module.css';
 
 export default function Nav() {
   return (
-    <nav className="w-full flex items-center p-4 border-b-solid border-b-2 border-gray-200">
-      <FornaxLogo />
-      <h2 className={`ms-4 inline-block text-xl ${styles.pitchScriptGenerator}`}>Pitch Script Generator</h2>
+    <nav className="w-full flex justify-between p-4 border-b-solid border-b-2 border-gray-200">
+      <div className="flex items-center">
+        <FornaxLogo />
+        <h2 className={`ms-4 inline-block text-xl ${styles.pitchScriptGenerator}`}>Pitch Script Generator</h2>
+      </div>
+      <div>
+        <button className="rounded-md border-2 border-gray-200 py-1 px-2 text-sm me-2">Share</button>
+        <button className="rounded-md text-white bg-violet-950 py-1 px-2 text-sm">Copy to Clipboard</button>
+      </div>
     </nav>
   );
 }
