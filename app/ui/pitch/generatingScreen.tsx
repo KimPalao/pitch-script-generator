7fe44cc53d8f2682977e24bc02275567a8f50aa6
Kim Palao <kimfarhantpalao@gmail.com>
2024-07-15 20:26:54 +0800

    Add loading bar animation

--- app/ui/pitch/generatingScreen.tsx
+++ app/ui/pitch/generatingScreen.tsx
@@ -1,7 +1,25 @@
 import { geologica } from '@/app/fonts';
 import styles from './generatingScreen.module.css';
+import { useEffect, useRef, useState } from 'react';
 
 export default function GeneratingScreen() {
+  const [loadingBarProgress, setLoadingBarProgress] = useState(0);
+  const startingTime = useRef(Date.now());
+
+  const updateLoadingBar = () => {
+    const currentTime = Date.now();
+    const timeElapsed = currentTime - startingTime.current;
+    let progress = Math.log(timeElapsed) * 10;
+    if (progress >= 100) {
+      progress = 99;
+    }
+    setLoadingBarProgress(progress);
+  };
+
+  useEffect(() => {
+    setInterval(updateLoadingBar, 100);
+  },);
+
   return (
     <div className={`${styles.generatingScreen} flex flex-col`}>
       <p className={`${geologica.className} font-medium text-2xl mb-4`}>
@@ -9,7 +27,7 @@ export default function GeneratingScreen() {
         This may take a few seconds.
       </p>
       <div className={`${styles.loadingBar} rounded-full`}>
-        <div className={`${styles.loadingBarProgress} rounded-full`}></div>
+        <div className={`${styles.loadingBarProgress} rounded-full`} style={{ width: `${loadingBarProgress}%` }}></div>
       </div>
     </div>
   );
