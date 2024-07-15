c74713618bca2db2ba36af51f59b18db64958212
Kim Palao <kimfarhantpalao@gmail.com>
2024-07-15 23:46:18 +0800

    Add copy pitch functin to card

--- app/ui/pitch/card.tsx
+++ app/ui/pitch/card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Card({
   title,
   content,
@@ -7,6 +9,16 @@ export default function Card({
   content?: string;
   time?: string;
 }) {
+  const [copied, setCopied] = useState(false);
+  const copyPitch = () => {
+    let text = ``;
+    text += `# ${title}\n\n`;
+    text += `**${time}**\n\n`;
+    text += `${content}\n\n`;
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
+
   return (
     <section className='px-4 py-2 border-solid border-2 border-gray-200 rounded-lg text mb-4 bg-white'>
       <div className="flex flex-row justify-between">
@@ -16,9 +28,10 @@ export default function Card({
             <span className='ms-2 text-sm'>{time}</span>
           }
         </div>
-        {content && <div>
-          <span>&#128203;</span>
-          <span>Refresh</span>
+        {content && <div className="text-xl">
+          {copied ? <button onClick={copyPitch} className="inline-block">&#x2713;</button>
+            : <button onClick={copyPitch} className="inline-block">&#128203;</button>}
+          <span>&#8634;</span>
         </div>}
       </div>
       {content?.length && <p className="text-gray-500">
